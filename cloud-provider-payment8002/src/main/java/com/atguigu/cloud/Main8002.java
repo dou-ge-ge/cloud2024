a4cb3c42b3d2c132fc3d882fb5e11aadc19a572c
dou-ge-ge <574521374@qq.com>
2024-06-03 08:41:39 +0800

    main update

---
+++ cloud-provider-payment8002/src/main/java/com/atguigu/cloud/Main8002.java
@@ -0,0 +1,15 @@
+package com.atguigu.cloud;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@MapperScan("com.atguigu.cloud.mapper")
+public class Main8002 {
+    public static void main(String[] args) {
+        SpringApplication.run(Main8002.class,args);
+    }
+}
